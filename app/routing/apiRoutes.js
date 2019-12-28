073e8eb82f92fddff77912834d60f6e8b6d3c77c
Emilio Valladares <vallae.wumbo@gmail.com>
2019-12-28 15:33:18 -0800

    Logic for calculating best match added to apiRoutes.js.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,55 @@
 var friends = require("../data/friends.js");
 
-module.exports = function(app){
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res){
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
+    });
+
+    app.post("/api/friends", function (req, res) {
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+        console.log(req.body);
+
+        // results of user survey POST are parsed
+        var userData = req.body;
+        var userScores = userData.scores;
+
+        console.log(userScores);
+
+        var totalDifference = 0;
+
+        // Looping through all friend possibilities
+        for (var i = 0; i < friends.length; i++) {
+
+            console.log(friends[i]);
+            totalDifference = 0;
+
+            // Looping through scores
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+
+                // Calculating dif and adding to totalDifference
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+                if (totalDifference <= bestMatch.friendDifference) {
+
+                    // Resets best match to nre friend
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = friends[i].friendDifference;
+
+                }
+            }
+        }
+
+        friends.push(userData);
+
+        // Return JSON with the user's best match
+        res.json(bestMatch);
     });
 }
